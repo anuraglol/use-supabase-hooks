adb2b285f52a1c8893df0f56cf0b393af931e965
Anurag <kr.anurag24@gmail.com>
2022-08-07 08:57:19 +0530

    add type declarations, testing

---
+++ example/pages/index.tsx
@@ -0,0 +1,8 @@
+import type { NextPage } from "next";
+import { useUser, useReadData, useSupabase } from "use-supabase-auth";
+
+const Home: NextPage = () => {
+  return <div></div>;
+};
+
+export default Home;
--- src/hooks/useSupabase.ts
+++ src/hooks/useSupabase.ts
@@ -2,8 +2,8 @@ import { createClient, SupabaseClient } from "@supabase/supabase-js";
 import { useState } from "react";
 
 const useSupabase = (SUPABASE_URL: string, SUPABASE_PK: string) => {
+  const [supabase, setSupabase] = useState<SupabaseClient>();
   const client = createClient(SUPABASE_URL, SUPABASE_PK);
-  const [supabase, setSupabase] = useState<SupabaseClient>(client);
 
   setSupabase(client);
   return supabase;
